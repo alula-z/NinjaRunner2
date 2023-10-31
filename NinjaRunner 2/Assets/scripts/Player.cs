6e68aec7f63b302c23380c939b7f8a3ed5375f67
alula-z <azeruesenay14@gmail.com>
2023-10-31 00:57:46 -0400

    fixed running error

---
+++ NinjaRunner 2/Assets/scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButton("Left"))
+        {
+       
+            transform.position -= new Vector3(1, 0, 0)* speed * Time.deltaTime;
+           
+        }
+        if (Input.GetButton("Right"))
+        {
+            
+            transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(rb.velocity.x, jumpForce));
+
+        }
+    }
+
+    
+}
